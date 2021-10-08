7aacb7241b1fc2a9633752ce0532d4de9ec82f78
Zander <zanpres@gmail.com>
2021-10-08 01:03:40 -0400

    Removed comment from command handler

--- src/functions/handleCommands.js
+++ src/functions/handleCommands.js
@@ -14,8 +14,7 @@ module.exports = (client) => {
             const commandFiles = fs.readdirSync(`${path}/${folder}`).filter(file => file.endsWith('.js'));
             for (const file of commandFiles) {
                 const command = require(`../commands/${folder}/${file}`);
-                // Set a new item in the Collection
-                // With the key as the command name and the value as the exported module
+
                 client.commands.set(command.data.name, command);
                 client.commandArray.push(command.data.toJSON());
             }
