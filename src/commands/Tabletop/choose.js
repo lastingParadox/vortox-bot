a563d8ecb7bb98e37106855da62d82d8c57cad64
lastingParadox <zanpres@gmail.com>
2022-08-17 00:38:45 -0400

    Fixed 8ball changes.

--- src/commands/Tabletop/choose.js
+++ src/commands/Tabletop/choose.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
-const fs = require('fs');
 
 module.exports = {
     data: new SlashCommandBuilder()
