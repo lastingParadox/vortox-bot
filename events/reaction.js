75cf950805ba92738058df291e44c987ea4d6c8c
retribyte <treyroemer@gmail.com>
2022-10-27 23:22:38 -0500

    Fixes #21, create VortoxReact object, edit existing reaction events to use VortoxReact values, add reaction event for bot messages

--- events/reaction.js
+++ events/reaction.js
@@ -1,14 +1,26 @@
+const { VortoxReact } = require('../utilities/enums');
+
 module.exports = {
     name: 'messageCreate',
-    execute(message) {
-        if (message.author.bot) return false;
+    async execute(message) {
+        if (!message.author.bot) {
+            if (message.content.toLowerCase().includes("orb")) {
+                await message.react(VortoxReact.ORB);
+            }
 
-        if (message.content.toLowerCase().includes("orb")) {
-            message.react(`🟠`).then();
+            if (message.content.toLowerCase().includes("squoat")) {
+                await message.react(VortoxReact.SQUOAT);
+            }
+
         }
-
-        if (message.content.toLowerCase().includes("squoat")) {
-            message.react(`🦀`).then();
+        else if (message.author.bot) {
+            if (message.embeds.length > 0) {
+                const embed = message.embeds[0];
+                if (embed.description.toLowerCase().includes("vote")) {
+                    for (let i of VortoxReact.VOTE)
+                        await message.react(i);
+                }
+            }
         }
     },
 };
